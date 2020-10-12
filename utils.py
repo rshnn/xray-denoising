396014209ca1b87ead12f57ea473bbac48ebb9e1
rshn-doomhammer <roshanpatel06@gmail.com>
2020-10-12 19:14:07 -0400

    completes dataset generator and prototype of training loop

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ Utilities for image processing and data munging
 import os
 import numpy as np 
 from PIL import Image 
+import tensorflow as tf 
 
 
 def corrupt_image_snp(img, ratio=0.5, amount=0.004):
@@ -79,3 +80,58 @@ def build_corrupted_dataset(root_dir, dest_dir, noise_fn, noise_fn_params):
         img_c.save(new_pth)
         
         
+        
+
+
+        
+        
+##
+## Functions for tf.Dataset serving 
+##
+
+# Currently: 
+#   + No image augmentation 
+#   + No use of image channels...just grayscale 
+
+def parse_function(filename): 
+    """ Returns tuple.  (input, output) 
+    
+    Input is corrupted image.   
+    Output is uncorrupted image.  Same filename. Diff folder. 
+    """
+
+    img_string = tf.io.read_file(filename) 
+    img_decoded = tf.image.decode_png(img_string)  
+    img_normed = img_decoded / 255
+
+    fn_split = tf.strings.split(filename, '/')
+    orig_filename = fn_split[0] + "/dataset128/" + fn_split[2]
+
+    out_img_string = tf.io.read_file(orig_filename) 
+    out_img_decoded = tf.image.decode_png(out_img_string)  
+    out_img_normed = out_img_decoded / 255
+
+    return img_normed, out_img_normed
+
+
+
+
+def create_dataset(filenames, SHUFFLE_BUFFER_SIZE, 
+                  AUTOTUNE, BATCH_SIZE): 
+    """ Init a tf.Dataset 
+    """   
+    
+    dataset = tf.data.Dataset.from_tensor_slices((filenames))
+    dataset = dataset.map(parse_function, num_parallel_calls=AUTOTUNE)
+    
+    dataset = dataset.batch(BATCH_SIZE)
+    dataset = dataset.prefetch(buffer_size=AUTOTUNE)
+    
+    return dataset
+
+
+
+def get_filenames_list(input_directory): 
+
+    filenames = os.listdir(os.path.abspath(input_directory)) 
+    return [input_directory + fn for fn in filenames]
